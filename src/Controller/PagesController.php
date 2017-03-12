cf63cc57a3ebe55073a9e4fdb71b05bb58d8845c
meru <s13a1001@bene.fit.ac.jp>
2017-03-12 11:13:25 +0900

    無視しておっけー

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -67,4 +67,9 @@ class PagesController extends AppController
             throw new NotFoundException();
         }
     }
+
+    public function isAuthorized($user)
+    {
+        return false;
+    }
 }
