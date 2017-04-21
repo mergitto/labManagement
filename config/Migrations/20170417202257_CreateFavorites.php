1d031dfe63f74b0bfcfd0094c4f5df0ed26a5c09
akagi <akagi@sgl14.cs.fit.ac.jp>
2017-04-21 19:32:27 +0900

    Migrationファイルの追加

---
+++ config/Migrations/20170417202257_CreateFavorites.php
@@ -0,0 +1,33 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateFavorites extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('favorites');
+        $table->addColumn('favo', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => true,
+        ]);
+        $table->addColumn('user_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addColumn('attachment_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}
