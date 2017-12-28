f74da277d17898180f26240ecd7c7df159a78cf6
mergitto <mfm17101@bene.fit.ac.jp>
2017-12-28 23:31:59 +0900

    markdownのcheckboxの追加を一旦断念

--- webroot/js/markdown/editor.js
+++ webroot/js/markdown/editor.js
@@ -6,33 +6,21 @@ $(function() {
     // 文字が入力される度にmarkdown形式の表示を更新する
     $('#editor').keyup(function() {
         var src = $(this).val();
-
         var html = marked(src);
-
         $('#result').html(html);
-
         $('pre code').each(function(i, block) {
             hljs.highlightBlock(block);
         });
-        // ここで[x]を選択済みのチェックボックスに[ ]を未選択のチェックボックスに変換
-        html = html.replace(/\[x\]/gi, '<input type="checkbox" checked="checked">');
-        html = html.replace(/\[ \]/gi, '<input type="checkbox" >');
     });
 
     // ページ読み込み時にmarkdown形式の表示に変更する
     $('.urlComment').each(function(){
-      var comment = $(this).text();
-
-      var html = marked(comment);
-
+      var src = $(this).text();
+      var html = marked(src);
       $(this).html(html);
-
       $('pre code').each(function(i, block) {
           hljs.highlightBlock(block);
       });
-      // ここで[x]を選択済みのチェックボックスに[ ]を未選択のチェックボックスに変換
-      html = html.replace(/\[x\]/gi, '<input type="checkbox" checked="checked">');
-      html = html.replace(/\[ \]/gi, '<input type="checkbox" >');
     });
 
 
@@ -43,8 +31,5 @@ $(function() {
     $('pre code').each(function(i, block) {
         hljs.highlightBlock(block);
     });
-    // ここで[x]を選択済みのチェックボックスに[ ]を未選択のチェックボックスに変換
-    html = html.replace(/\[x\]/gi, '<input type="checkbox" checked="checked">');
-    html = html.replace(/\[ \]/gi, '<input type="checkbox" >');
 });
 
